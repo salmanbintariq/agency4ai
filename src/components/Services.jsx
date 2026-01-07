62ef8d54c1762a03272835f764b55bfd55333a9c
Salman Tariq <salmantariq8018@gmail.com>
2026-01-07 15:59:06 +0500

    feat(services): add services section layout

---
+++ src/components/Services.jsx
@@ -0,0 +1,36 @@
+import assets from "../assets/assets"
+
+const Services = () => {
+
+  const servicesData = [
+    {
+      title: "Advertising",
+      description: "We turn bold ideas into powerful digital solutions that connect, engage...",
+      icon: assets.ads_icon
+    },
+    {
+      title: "Content marketing",
+      description: "We help you execute your plan and deliver results.",
+      icon: assets.marketing_icon
+    },
+    {
+      title: "Content writing",
+      description: "We help you create a marketing strategy that drives results.",
+      icon: assets.content_icon
+    },
+    {
+      title: "Social media",
+      description: "We help you build a strong social media presence and engage with your audience.",
+      icon: assets.social_icon
+    },
+  ]
+
+
+  return (
+    <div id="services" className="relative flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white">
+
+    </div>
+  )
+}
+
+export default Services
